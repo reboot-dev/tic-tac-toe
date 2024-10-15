811353f0fa7e17ad280e7428381d94c3649d4207
Benjamin Hindman <benjamin.hindman@gmail.com>
2024-10-15 18:16:15 +0000

    Implement backend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,40 @@
-import { Application } from "@reboot-dev/reboot";
+import { PartialMessage } from "@bufbuild/protobuf";
+import { Application, ReaderContext, WriterContext } from "@reboot-dev/reboot";
+import {
+  Game,
+  HistoryRequest,
+  HistoryResponse,
+  UpdateRequest,
+  UpdateResponse,
+} from "../../api/tic_tac_toe/v1/game_rbt.js";
 
-const initialize = async (context) => {};
+
+class GameServicer extends Game.Interface {
+
+  async history(
+    context: ReaderContext,
+    { moves }: Game.State,
+    _request: HistoryRequest
+  ): Promise<PartialMessage<HistoryResponse>> {
+    return { moves };
+  }
+
+  async update(
+    context: WriterContext,
+    state: Game.State,
+    { moves }: UpdateRequest
+  ): Promise<PartialMessage<UpdateResponse>> {
+    state.moves = moves;
+    return {};
+  }
+}
+
+
+const initialize = async (context) => {
+  await Game.construct({ id: "GAME" }).idempotently().update(context, { moves: [] });
+};
 
 new Application({
-  servicers: [],
+  servicers: [GameServicer],
   initialize,
 }).run();
